9e9285dd5940f84bffc68953c8ba68b66930b93f
Saravanabalagi <saravanabalagi@hotmail.com>
2018-12-20 15:49:54 +0000

    Code Refactoring for better organization

--- src/components/graph/drawpad.js
+++ src/components/graph/drawpad.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {PaintCanvas} from '../models/canvas';
+import {PaintCanvas} from '../../core/models/canvas';
 import {Button} from "bloomer";
 
 import './drawpad.css';
